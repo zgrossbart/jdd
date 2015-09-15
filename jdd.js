bd481f94e3930b91eac6592519f2f51fd9cb0ad1
Zack Grossbart <zack@grossbart.com>
2015-09-15 14:43:25 -0400

    We can now diff two JSON document and generate correct diffs with line numbers.

--- jdd.js
+++ jdd.js
@@ -1,6 +1,126 @@
 'use strict';
 
 var jdd = {
+
+    diffs: [],
+
+    findDiffs: function(/*Object*/ config1, /*Object*/ data1, /*Object*/ config2, /*Object*/ data2) {
+       config1.currentPath.push('/');
+       config2.currentPath.push('/');
+       if (data1.length < data2.length) {
+           /*
+            * This means the second data has more properties than the first.
+            * We need to find the extra ones and create diffs for them.
+            */
+           _.each(data2, function(val, key) {
+               if (!data1.hasOwnProperty(key)) {
+                   jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                                   config2, jdd.generatePath(config2, '/' + key),
+                                                   'Wrong number of items'));
+               }
+           });
+       }
+
+       /*
+        * Now we're going to look for all the properties in object one and
+        * compare them to object two
+        */
+       _.each(data1, function(val, key) {
+           config1.currentPath.push(key);
+
+           if (!data2.hasOwnProperty(key)) {
+               /*
+                * This means that the first data has a property which
+                * isn't present in the second data
+                */
+               console.log('config1.currentPath: ' + jdd.generatePath(config1));
+               console.log('config2.currentPath: ' + jdd.generatePath(config2));
+               jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                               config2, jdd.generatePath(config2),
+                                               'Missing property (' + key + ') from right side'));
+            } else {
+                config2.currentPath.push(key);
+            
+                jdd.diffVal(data1[key], config1, data2[key], config2);
+                config2.currentPath.pop();
+            }
+            config1.currentPath.pop();
+       });
+
+       config1.currentPath.pop();
+       config2.currentPath.pop();
+
+       /*
+        * Now we want to look at all the properties in object two that
+        * weren't in object one and generate diffs for them.
+        */
+       _.each(data2, function(val, key) {
+           if (!data1.hasOwnProperty(key)) {
+               jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                               config2, jdd.generatePath(config2, key),
+                                               'Missing property (' + key + ') from left side'));
+           }
+
+       });
+
+    },
+
+    diffVal: function(val1, config1, val2, config2) { 
+
+        if (_.isArray(val1)) {
+            if (!_.isArray(val2)) {
+               jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                               config2, jdd.generatePath(config2),
+                                               'Both types should be arrays'));
+            }
+            _.each(val1, function(arrayVal, index) {
+                config1.currentPath.push('/[' + index + ']');
+                config2.currentPath.push('/[' + index + ']');
+                jdd.diffVal(val1[index], config1, val2[index], config2);
+                config1.currentPath.pop();
+                config2.currentPath.pop();
+            });
+        } else if (_.isObject(val1)) {
+            if (!_.isObject(val2)) {
+               jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                               config2, jdd.generatePath(config2),
+                                               'Both types should be objects'));
+            }
+
+            jdd.findDiffs(config1, val1, config2, val2);
+        } else if (_.isString(val1)) {
+            if (!_.isString(val2)) {
+                jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                                config2, jdd.generatePath(config2),
+                                               'Both types should be objects'));
+            } else if (val1 !== val2) {
+                jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                                config2, jdd.generatePath(config2),
+                                               'Both sides should be equal strings'));
+            }
+        } else if (_.isNumber(val1)) {
+            if (!_.isNumber(val2)) {
+                jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                                config2, jdd.generatePath(config2),
+                                               'Both types should be numbers'));
+            } else if (val1 !== val2) {
+                jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                                config2, jdd.generatePath(config2),
+                                               'Both sides should be equal numbers'));
+            }
+        } else if (_.isBoolean(val1)) {
+            if (!_.isBoolean(val2)) {
+                jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                                config2, jdd.generatePath(config2),
+                                                'Both types should be booleans'));
+            } else if (val1 !== val2) {
+                jdd.diffs.push(jdd.generateDiff(config1, jdd.generatePath(config1),
+                                                config2, jdd.generatePath(config2),
+                                                'Both sides should be equal booleans'));
+            }
+        } 
+    },
+
     formatAndDecorate: function(/*Object*/ config, /*Object*/ data) {
         jdd.startObject(config);
         config.currentPath.push('/');
@@ -31,6 +151,17 @@ var jdd = {
     startObject: function(config) {
         config.indent++;
         config.out += '{';
+
+        if (config.paths.length === 0) {
+            /*
+             * Then we are at the top of the object and we want to add 
+             * a path for it.
+             */
+            config.paths.push({
+                path: jdd.generatePath(config),
+                line: config.line
+            });
+        }
         
         if (config.indent === 0) {
             config.indent++;
@@ -91,7 +222,11 @@ var jdd = {
             s += '/' + prop;
         }
 
-        return s;
+        if (s.length === 0) {
+            return '/';
+        } else {
+            return s;
+        }
     },
 
     newLine: function(config) {
@@ -113,10 +248,26 @@ var jdd = {
         return props;
     },
 
-    generateDiff: function(/*int*/ line1, /*int*/ line2, /*String*/ msg) {
+    generateDiff: function(config1, path1, config2, path2, /*String*/ msg) {
+        var pathObj1 = _.find(config1.paths, function(path) {
+            return path.path === path1;
+        });
+
+        var pathObj2 = _.find(config2.paths, function(path) {
+            return path.path === path2;
+        });
+
+        if (!pathObj1) {
+            throw 'Unable to find line number for(' + msg + '): ' + path1;
+        }
+
+        if (!pathObj2) {
+            throw 'Unable to find line number for(' + msg + '): ' + path2;
+        }
+
         return {
-            line1: line1,
-            line2: line2,
+            path1: pathObj1,
+            path2: pathObj2,
             msg: msg
         }
     },
@@ -169,16 +320,19 @@ var jdd = {
 jQuery(document).ready(function() {
     //console.log('data: ' + JSON.stringify(DATA));
     var config = jdd.createConfig();
-
     jdd.formatAndDecorate(config, DATA);
     $('#out').text(config.out);
-    console.log('paths: ' + JSON.stringify(config.paths));
-
-
+    
     var config2 = jdd.createConfig();
-
     jdd.formatAndDecorate(config2, DATA2);
     $('#out2').text(config2.out);
 
     jdd.formatPRETags();
+
+    config.currentPath = [];
+    config2.currentPath = [];
+
+    jdd.findDiffs(config, DATA, config2, DATA2);
+
+    console.log('diffs: ' + JSON.stringify(jdd.diffs));
 });
