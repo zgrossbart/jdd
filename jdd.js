b6930ee4c863a017de7770cec0eee9ed42129c23
Surana, Nitin <nisurana@ebay.com>
2019-05-03 19:11:14 -0700

    Fixed not working click handlers and separated polyfill and other code not related to comparison

--- jdd.js
+++ jdd.js
@@ -677,7 +677,7 @@ function Jdd($parent) {
             prev.click(function(e) {
                 e.preventDefault();
                 this.highlightPrevDiff();
-            });
+            }.bind(this));
             buttons.append(prev);
 
             buttons.append('<span id="prevNextLabel"></span>');
@@ -686,7 +686,7 @@ function Jdd($parent) {
             next.click(function(e) {
                 e.preventDefault();
                 this.highlightNextDiff();
-            });
+            }.bind(this));
             buttons.append(next);
 
             $parent.find('ul.toolbar').append(buttons);
@@ -746,9 +746,9 @@ function Jdd($parent) {
 
                 li.click(function() {
                     this.scrollToDiff(diff);
-                });
+                }.bind(this));
 
-            });
+            }.bind(this));
         },
 
         /**
@@ -771,7 +771,7 @@ function Jdd($parent) {
                 if (left.indexOf(diff.path1.line) === -1) {
                     $parent.find('pre.left div.line' + diff.path1.line + ' span.code').click(function() {
                         this.handleDiffClick(diff.path1.line, this.LEFT);
-                    });
+                    }.bind(this));
                     left.push(diff.path1.line);
                 }
 
@@ -779,10 +779,10 @@ function Jdd($parent) {
                 if (right.indexOf(diff.path2.line) === -1) {
                     $parent.find('pre.right div.line' + diff.path2.line + ' span.code').click(function() {
                         this.handleDiffClick(diff.path2.line, this.RIGHT);
-                    });
+                    }.bind(this));
                     right.push(diff.path2.line);
                 }
-            });
+            }.bind(this));
 
             this.diffs = this.diffs.sort(function(a, b) {
                 return a.path1.line - b.path1.line;
@@ -856,7 +856,7 @@ function Jdd($parent) {
             report.append(newDiff);
             newDiff.click(function() {
                 this.setupNewDiff();
-            });
+            }.bind(this));
 
             if (this.diffs.length === 0) {
                 report.append('<span>The two files were semantically  identical.</span>');
@@ -1084,140 +1084,3 @@ function Jdd($parent) {
     }
 };
 
-
-jQuery(document).ready(function() {
-    const jddObj = new Jdd($('body'));
-    $('#compare').click(function() {
-        jddObj.compare();
-    });
-
-    if (jddObj.getParameterByName('left')) {
-        $('#textarealeft').val(jddObj.getParameterByName('left'));
-    }
-
-    if (jddObj.getParameterByName('right')) {
-        $('#textarearight').val(jddObj.getParameterByName('right'));
-    }
-
-    if (jddObj.getParameterByName('left') && jddObj.getParameterByName('right')) {
-        jddObj.compare();
-    }
-
-
-    $('#sample').click(function(e) {
-        e.preventDefault();
-        jddObj.loadSampleData();
-    });
-
-    $(document).keydown(function(event) {
-        if (event.keyCode === 78 || event.keyCode === 39) {
-            /*
-             * The N key or right arrow key
-             */
-            jddObj.highlightNextDiff();
-        } else if (event.keyCode === 80 || event.keyCode === 37) {
-            /*
-             * The P key or left arrow key
-             */
-            jddObj.highlightPrevDiff();
-        }
-    });
-});
-
-// polyfills
-
-// Array.prototype.find
-// https://tc39.github.io/ecma262/#sec-array.prototype.find
-if (!Array.prototype.find) {
-    Object.defineProperty(Array.prototype, 'find', {
-        value: function(predicate) {
-            // 1. Let O be ? ToObject(this value).
-            if (this === null) {
-                throw new TypeError('"this" is null or not defined');
-            }
-
-            var o = Object(this);
-
-            // 2. Let len be ? ToLength(? Get(O, "length")).
-            var len = o.length >>> 0;
-
-            // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-            if (typeof predicate !== 'function') {
-                throw new TypeError('predicate must be a function');
-            }
-
-            // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-            var thisArg = arguments[1];
-
-            // 5. Let k be 0.
-            var k = 0;
-
-            // 6. Repeat, while k < len
-            while (k < len) {
-                // a. Let Pk be ! ToString(k).
-                // b. Let kValue be ? Get(O, Pk).
-                // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
-                // d. If testResult is true, return kValue.
-                var kValue = o[k];
-                if (predicate.call(thisArg, kValue, k, o)) {
-                    return kValue;
-                }
-                // e. Increase k by 1.
-                k++;
-            }
-
-            // 7. Return undefined.
-            return undefined;
-        },
-        configurable: true,
-        writable: true
-    });
-}
-
-// Array.prototype.findIndex
-// https://tc39.github.io/ecma262/#sec-array.prototype.findIndex
-if (!Array.prototype.findIndex) {
-    Object.defineProperty(Array.prototype, 'findIndex', {
-        value: function(predicate) {
-            // 1. Let O be ? ToObject(this value).
-            if (this === null) {
-                throw new TypeError('"this" is null or not defined');
-            }
-
-            var o = Object(this);
-
-            // 2. Let len be ? ToLength(? Get(O, "length")).
-            var len = o.length >>> 0;
-
-            // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-            if (typeof predicate !== 'function') {
-                throw new TypeError('predicate must be a function');
-            }
-
-            // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-            var thisArg = arguments[1];
-
-            // 5. Let k be 0.
-            var k = 0;
-
-            // 6. Repeat, while k < len
-            while (k < len) {
-                // a. Let Pk be ! ToString(k).
-                // b. Let kValue be ? Get(O, Pk).
-                // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
-                // d. If testResult is true, return k.
-                var kValue = o[k];
-                if (predicate.call(thisArg, kValue, k, o)) {
-                    return k;
-                }
-                // e. Increase k by 1.
-                k++;
-            }
-
-            // 7. Return -1.
-            return -1;
-        },
-        configurable: true,
-        writable: true
-    });
-}
