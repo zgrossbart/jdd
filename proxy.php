b4938411c887d04932b1fd2859903817e94a35a6
zgrossbart <zack@grossbart.com>
2023-02-28 06:17:38 -0500

    Updated the proxy to fix a potential security vulnerability.

--- proxy.php
+++ proxy.php
@@ -17,7 +17,7 @@ if (($scheme !== "http" && $scheme !== "https")) {
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 $data = curl_exec($ch);
 $info = curl_getinfo($ch);
 curl_close($ch);
